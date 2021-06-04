0517346934f696248b174000f3d1fe112fb6b5ac
Alvaro <wallealvaro@gmail.com>
2021-06-03 22:31:36 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Alvaro</code>
+          <code>Alvaro.</code>
         </p>
         <a
           className="App-link"
